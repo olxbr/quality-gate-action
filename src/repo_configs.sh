afe4215a015235bc73baad1a15326e777a60d371
Roger Fernandes <roger.osbourne@gmail.com>
2023-08-22 11:34:14 -0300

    feat(repo configs): validate if CODEOWNERS file exists

--- src/repo_configs.sh
+++ src/repo_configs.sh
@@ -6,6 +6,15 @@ source ${ACTION_PATH}/src/github_client.sh
 export GH_TOKEN=${GITHUB_TOKEN}
 export REPOSITORY=${GITHUB_REPOSITORY}
 
+# Function to validate if CODEOWNERS file exists
+function _validate_codeowners() {
+    if [ -f "CODEOWNERS" ] || [ -f "docs/CODEOWNERS" ] || [ -f ".github/CODEOWNERS" ]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
 # Function to check repo configs
 function _check_repo_configs() {
     _log "${C_WHT}Checking Repository Configurations...${C_END}"
@@ -24,6 +33,15 @@ function _check_repo_configs() {
     if [[ $(jq 'length > 0' <<<"$rules") == true ]]; then
         is_required_code_review_approval=$(jq -r 'any(.[]; .required_approving_review_count > 0)' <<<"$rules")
         is_required_code_owner_approval=$(jq -r 'any(.[]; .require_code_owner_review == true)' <<<"$rules")
+
+        if [[ $is_required_code_owner_approval == true ]]; then
+            if [[ $(_validate_codeowners) == false ]]; then
+                _log warn "${C_YEL}CODEOWNERS file not found!${C_END}"
+                is_required_code_owner_approval=false
+            fi
+        else
+            _log warn "${C_YEL}[Required Code Owner Approval] rule is disabled!${C_END}"
+        fi
 
         _log "${C_WHT}Required Code Review Approval:${C_END} ${is_required_code_review_approval}"
         _log "${C_WHT}Required Code Owner Approval:${C_END} ${is_required_code_owner_approval}"
