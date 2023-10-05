48c826c67ce9899f2a540f175e21d1f54e203bef
Roger Fernandes <roger.osbourne@gmail.com>
2023-10-05 12:54:02 -0300

    feat: apply retry in check unit test func

--- src/unit_test.sh
+++ src/unit_test.sh
@@ -23,6 +23,35 @@ function _get_workflow_run_id() {
     $succeeded
 }
 
+function _check_unit_test_status() {
+    _log "${C_WHT}Waiting for Unit Test...${C_END}"
+    local succeeded=false
+
+    quality_gate_step=$(_get_quality_gate_unit_test_step "$workflow_run_id")
+
+    status=$(jq -r '.status' <<<"$quality_gate_step")
+    conclusion=$(jq -r '.conclusion' <<<"$quality_gate_step")
+
+    if [[ $status == "completed" ]]; then
+        succeeded=true
+        if [[ $conclusion == "success" ]]; then
+            _log "${C_WHT}Unit Test completed successfully!${C_END}"
+            is_unit_tests_pass=true
+        else
+            message="Unit Test Failed!"
+            _log warn "${C_YEL}${message}${C_END}"
+            _insert_warning_message unit_tests_warn_msg "⚠️ ${message}"
+
+            _log "${C_WHT}Quality Gate Step Status:${C_END} ${status}"
+            _log "${C_WHT}Quality Gate Step Conclusion:${C_END} ${conclusion}"
+        fi
+    else
+        _log "${C_WHT}Unit Test not completed yet!${C_END}"
+    fi
+
+    $succeeded
+}
+
 # Function to check unit tests
 function _check_unit_test() {
     skip_unit_tests=$(_has_gate_to_skip "unit_test")
@@ -39,41 +68,7 @@ function _check_unit_test() {
 
         if [[ -n "$workflow_run_id" ]]; then
             _log "${C_WHT}Workflow Run ID:${C_END} ${workflow_run_id}"
-
-            # The time of UNIT_TEST_CHECK_TIMEOUT in minutes divide by 10 seconds
-            local sleep=10
-            local count=0
-            local retries=$(echo "($UNIT_TEST_CHECK_TIMEOUT * 60) / $sleep" | bc)
-            _log "${C_WHT}Unit Test Check Timeout:${C_END} ${UNIT_TEST_CHECK_TIMEOUT} minutes"
-
-            _log "${C_WHT}Waiting for Unit Test...${C_END}"
-            while [[ $count -lt $retries ]]; do
-                quality_gate_step=$(_get_quality_gate_unit_test_step "$workflow_run_id")
-
-                status=$(jq -r '.status' <<<"$quality_gate_step")
-                conclusion=$(jq -r '.conclusion' <<<"$quality_gate_step")
-
-                if [[ $status == "completed" ]]; then
-                    if [[ $conclusion == "success" ]]; then
-                        _log "${C_WHT}Unit Test completed successfully!${C_END}"
-                        is_unit_tests_pass=true
-                        break
-                    else
-                        message="Unit Test Failed!"
-                        _log warn "${C_YEL}${message}${C_END}"
-                        _insert_warning_message unit_tests_warn_msg "⚠️ ${message}"
-
-                        _log "${C_WHT}Quality Gate Step Status:${C_END} ${status}"
-                        _log "${C_WHT}Quality Gate Step Conclusion:${C_END} ${conclusion}"
-                        break
-                    fi
-                else
-                    _log "${C_WHT}Unit Test not completed yet!${C_END}"
-                    count=$((count + 1))
-                    sleep $sleep
-                fi
-            done
-
+            _retry_with_delay _check_unit_test_status
         else
             message="Step ($UNIT_TEST_STEP_NAME) not found!"
             _log warn "${C_YEL}${message}${C_END}"
