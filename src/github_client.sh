21864ad031c866f308deb324fc1b23ac20120a05
Jefferson Braga <jeffbraga@gmail.com>
2023-10-25 11:29:18 -0300

    Debug: Log

--- src/github_client.sh
+++ src/github_client.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+source "${ACTION_PATH}/src/utils.sh"
+
 function _gh_client() {
-    _log debug "${C_WHT}Executing command: gh api $@${C_END}"
+    _log "${C_WHT}Executing command: gh api $@${C_END}"
     gh api \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "$@"
+        $@
 }
 
 function _get_ruleset_ids() {
