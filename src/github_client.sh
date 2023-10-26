09d6a48ab2f99c50074b9bd321397906dbaee76b
Jefferson Braga <jeffbraga@gmail.com>
2023-10-26 20:50:21 -0300

    Update: Keep same struct

--- src/github_client.sh
+++ src/github_client.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source "${ACTION_PATH}/src/utils.sh"
-
 function _gh_client() {
     _log debug "${C_WHT}Executing command:${C_END} gh api $@"
     result=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "$@")
@@ -20,7 +18,9 @@ function _get_rules() {
     local ruleset_ids=$1
 
     if [ -n "$ruleset_ids" ]; then
-        rules=$(eval _gh_client /repos/"$REPOSITORY"/rules/branches/$GITHUB_DEFAULT_BRANCH | jq ".[] | select(.type == \"pull_request\" and (.ruleset_id == ($ruleset_ids) )) | .parameters")
+        rules=$(_gh_client \
+            --jq ".[] | select(.type == \"pull_request\" and (.ruleset_id == ($ruleset_ids) )) | .parameters" \
+            /repos/"$REPOSITORY"/rules/branches/$GITHUB_DEFAULT_BRANCH)
 
         echo $rules | jq -s
     fi
@@ -29,7 +29,9 @@ function _get_rules() {
 function _get_pr_report_comment_id() {
     local comment_title="# Quality Gate"
 
-    comment_id=$(eval _gh_client "/repos/$REPOSITORY/issues/$PR_NUMBER/comments?per_page=100" | jq "[.[] | select(.body | startswith(\"$comment_title\")) | .id][0]")
+    comment_id=$(_gh_client \
+        --jq "[.[] | select(.body | startswith(\"$comment_title\")) | .id][0]" \
+        "/repos/$REPOSITORY/issues/$PR_NUMBER/comments?per_page=100")
 
     echo "$comment_id"
 }
@@ -38,7 +40,11 @@ function _create_pr_report_comment() {
     local report=$1
 
     if [ -n "$report" ]; then
-        _gh_client --silent  -X POST -f body="$report" /repos/"$REPOSITORY"/issues/"$PR_NUMBER"/comments
+        _gh_client \
+            -X POST \
+            --silent \
+            -f body="$report" \
+            /repos/"$REPOSITORY"/issues/"$PR_NUMBER"/comments
     fi
 }
 
@@ -47,7 +53,11 @@ function _update_pr_report_comment() {
     local report=$2
 
     if [ -n "$comment_id" ] && [ -n "$report" ]; then
-        _gh_client --silent --method PATCH -f body="$report" /repos/"$REPOSITORY"/issues/comments/"$comment_id"
+        _gh_client \
+            --method PATCH \
+            --silent \
+            -f body="$report" \
+            /repos/"$REPOSITORY"/issues/comments/"$comment_id"
     fi
 }
 
@@ -62,8 +72,9 @@ function _get_quality_gate_unit_test_step() {
     local workflow_run_id=$1
 
     if [ -n "$workflow_run_id" ]; then
-        quality_gate_step=$(
-            _gh_client "/repos/$REPOSITORY/actions/runs/$workflow_run_id/jobs" | jq ".jobs[].steps[] | select(.name == \"$UNIT_TEST_STEP_NAME\")")
+        quality_gate_step=$(_gh_client \
+            --jq ".jobs[].steps[] | select(.name == \"$UNIT_TEST_STEP_NAME\")" \
+            "/repos/$REPOSITORY/actions/runs/$workflow_run_id/jobs")
         echo "$quality_gate_step"
     fi
 }
