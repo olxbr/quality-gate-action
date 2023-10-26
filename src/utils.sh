a07a83ee54c6ad10ef69ae182cd2deed95ed64fd
Jefferson Braga <jeffbraga@gmail.com>
2023-10-26 20:02:09 -0300

    Fix: Forgot to add REPOSITORY var

--- src/utils.sh
+++ src/utils.sh
@@ -22,13 +22,13 @@ function _log() {
     output=/dev/stdout
 
     case $1 in
-    erro) logLevel="${C_RED}[ERRO]${C_END}"; msg="${msg#erro*\ }";;
-    warn) logLevel="${C_YEL}[WARN]${C_END}"; msg="${msg#warn*\ }";;
-    debug)logLevel="${C_YEL}[DEBUG]${C_END}"; msg="${msg#debug*\ }"
-        [[ -n "$RUNNER_DEBUG" ]] &&
-            output=/dev/stderr ||
-            output=/dev/null;;
-    *)  logLevel="${C_WHT}[INFO]${C_END}";;
+        erro) logLevel="${C_RED}[ERRO]${C_END}"; msg="${msg#erro*\ }";;
+        warn) logLevel="${C_YEL}[WARN]${C_END}"; msg="${msg#warn*\ }";;
+        debug)logLevel="${C_YEL}[DEBUG]${C_END}"; msg="${msg#debug*\ }"
+            [[ -n "$RUNNER_DEBUG" ]] &&
+                output=/dev/stderr ||
+                output=/dev/null;;
+        *)  logLevel="${C_WHT}[INFO]${C_END}";;
     esac
 
     echo -e "$(date +"%d-%b-%Y %H:%M:%S") ${logLevel} - ${msg##*( )}${C_END}" > $output
