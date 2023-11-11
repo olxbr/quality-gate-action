e99b5e803f2aa9839f7a8b4596bced5d24e0a302
Jefferson Braga <jeffbraga@gmail.com>
2023-11-10 23:00:28 -0300

    Update: Keep same design code

--- src/submit_metrics.sh
+++ src/submit_metrics.sh
@@ -15,20 +15,23 @@ CURL_LOG="curl.log"
 CURL_ERR="curl.err"
 CURL_CMD="curl -sv --connect-timeout 5 --write-out '%{http_code}' -X POST -H 'Content-Type: application/json' -d '$DATA' '$ENDPOINT_URL' -o ${CURL_LOG} 2> ${CURL_ERR}"
 
-_log "Sending data to endpoint..."
-_log debug "Executing command: $CURL_CMD"
-CURL_RES=$(eval $CURL_CMD)
+## MAIN ##
+function _submit_metrics() {
+    _log "Sending data to endpoint..."
+    _log debug "Executing command: $CURL_CMD"
+    CURL_RES=$(eval $CURL_CMD)
 
-if [[ "${CURL_RES}" =~ [23].. ]]; then
-    _log "Data sent successfully to endpoint ${ENDPOINT_URL}. Status code: ${CURL_RES}"
-else
-    _log error "Failed to send data to endpoint ${ENDPOINT_URL}. Status code: ${CURL_RES}"
-fi
+    if [[ "${CURL_RES}" =~ [23].. ]]; then
+        _log "Data sent successfully to endpoint ${ENDPOINT_URL}. Status code: ${CURL_RES}"
+    else
+        _log error "Failed to send data to endpoint ${ENDPOINT_URL}. Status code: ${CURL_RES}"
+    fi
 
-_log debug "Response: $(cat ${CURL_LOG})"
-_log debug "Full log: $(cat ${CURL_ERR})"
+    _log debug "Response: $(cat ${CURL_LOG})"
+    _log debug "Full log: $(cat ${CURL_ERR})"
 
-env
+    env
 
-## Delete files if they exist
-rm -f "${CURL_LOG}" "${CURL_ERR}"
+    ## Delete files if they exist
+    rm -f "${CURL_LOG}" "${CURL_ERR}"
+}
