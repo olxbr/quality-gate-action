d5b37616671315f8701fede5e517454fed5a87bf
Jefferson Braga <jeffbraga@gmail.com>
2023-11-12 19:56:19 -0300

    Update: Add more datas

--- src/submit_metrics.sh
+++ src/submit_metrics.sh
@@ -13,6 +13,11 @@ DATA='{
     "workflow": "${GITHUB_WORKFLOW}",
     "run_id": "${GITHUB_RUN_ID}",
     "gates_to_skip": "${GATES_TO_SKIP}",
+    "num_commits": ${{ github.event.pull_request.commits }},
+    "num_changed_files": ${{ github.event.pull_request.changed_files }},
+    "num_additions": ${{ github.event.pull_request.additions }},
+    "num_deletions": ${{ github.event.pull_request.deletions }},
+    "created_at": "${{ github.event.pull_request.created_at }}",
     "value": 42
 }'
 
@@ -22,7 +27,7 @@ DATA=$(envsubst <<<"$DATA")
 # Send data to endpoint
 CURL_LOG="curl.log"
 CURL_ERR="curl.err"
-CURL_CMD="curl -sv --connect-timeout 5 --write-out '%{http_code}' -X POST -H 'Content-Type: application/json' -d '$DATA' '$ENDPOINT_URL' -o ${CURL_LOG} 2> ${CURL_ERR}"
+CURL_CMD="curl -sv --connect-timeout 5 --write-out '%{http_code}' -X POST -H 'Content-Type: application/json' '$ENDPOINT_URL' -d '$DATA' -o ${CURL_LOG} 2> ${CURL_ERR}"
 
 ## MAIN ##
 function _submit_metrics() {
