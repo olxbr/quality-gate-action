084b7226c33a3b0d54121544d92f16d347ae3a87
Roger Fernandes <roger.osbourne@gmail.com>
2024-01-02 12:39:58 -0300

    feat: change update to delete method

--- src/github_client.sh
+++ src/github_client.sh
@@ -9,8 +9,8 @@ function _gh_client() {
 
 function _get_ruleset_ids() {
     ruleset_ids=$(_gh_client \
-        /repos/"$REPOSITORY"/rulesets | \
-        jq -r '[.[] | select(.enforcement == "active") | .id] | join(",")' | \
+        /repos/"$REPOSITORY"/rulesets |
+        jq -r '[.[] | select(.enforcement == "active") | .id] | join(",")' |
         grep -v '^null$')
     _log debug "${C_WHT}Ruleset IDs actived:${C_END} ${ruleset_ids}"
     echo "$ruleset_ids"
@@ -21,49 +21,12 @@ function _get_rules() {
 
     if [ -n "$ruleset_ids" ]; then
         rules=$(_gh_client \
-            /repos/"$REPOSITORY"/rules/branches/$GITHUB_DEFAULT_BRANCH | \
-            jq -r ".[] | select(.type == \"pull_request\" and (.ruleset_id == ($ruleset_ids) )) | .parameters" | \
+            /repos/"$REPOSITORY"/rules/branches/$GITHUB_DEFAULT_BRANCH |
+            jq -r ".[] | select(.type == \"pull_request\" and (.ruleset_id == ($ruleset_ids) )) | .parameters" |
             grep -v '^null$')
 
         _log debug "${C_WHT}Rules found for id ${ruleset_ids}:${C_END} ${rules}"
         echo $rules
-    fi
-}
-
-function _get_pr_report_comment_id() {
-    local comment_title="# Quality Gate"
-
-    comment_id=$(_gh_client \
-        "/repos/$REPOSITORY/issues/$PR_NUMBER/comments?per_page=100" | \
-        jq -r --arg comment_title "$comment_title" \
-            '[.[] | select(.body | startswith($comment_title)) | .id][0]' | \
-        grep -v '^null$')
-
-    echo "$comment_id"
-}
-
-function _create_pr_report_comment() {
-    local report=$1
-
-    if [ -n "$report" ]; then
-        _gh_client \
-            -X POST \
-            --silent \
-            -f body="$report" \
-            /repos/"$REPOSITORY"/issues/"$PR_NUMBER"/comments
-    fi
-}
-
-function _update_pr_report_comment() {
-    local comment_id=$1
-    local report=$2
-
-    if [ -n "$comment_id" ] && [ -n "$report" ]; then
-        _gh_client \
-            --method PATCH \
-            --silent \
-            -f body="$report" \
-            /repos/"$REPOSITORY"/issues/comments/"$comment_id"
     fi
 }
 
@@ -79,11 +42,47 @@ function _get_quality_gate_unit_test_step() {
 
     if [ -n "$workflow_run_id" ]; then
         quality_gate_step=$(_gh_client \
-            "/repos/$REPOSITORY/actions/runs/$workflow_run_id/jobs" | \
+            "/repos/$REPOSITORY/actions/runs/$workflow_run_id/jobs" |
             jq --arg job_name "$UNIT_TEST_STEP_NAME" \
-                '.jobs[].steps[] | select(.name == $job_name)' | \
+                '.jobs[].steps[] | select(.name == $job_name)' |
             grep -v '^null$')
 
         echo "$quality_gate_step"
+    fi
+}
+
+function _get_pr_report_comment_id() {
+    local comment_title="# Quality Gate"
+
+    comment_id=$(_gh_client \
+        "/repos/$REPOSITORY/issues/$PR_NUMBER/comments?per_page=100" |
+        jq -r --arg comment_title "$comment_title" \
+            '[.[] | select(.body | startswith($comment_title)) | .id][0]' |
+        grep -v '^null$')
+
+    echo "$comment_id"
+}
+
+function _create_pr_report_comment() {
+    local report=$1
+
+    if [ -n "$report" ]; then
+        _gh_client \
+            --method POST \
+            --silent \
+            -f body="$report" \
+            /repos/"$REPOSITORY"/issues/"$PR_NUMBER"/comments
+    fi
+}
+
+function _delete_pr_report_comment() {
+    local comment_id=$1
+
+    if [ -n "$comment_id" ]; then
+        _gh_client \
+            --method DELETE \
+            --silent \
+            -f body="$report" \
+            /repos/"$REPOSITORY"/issues/comments/"$comment_id"
     fi
 }
