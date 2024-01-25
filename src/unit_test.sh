41fc0fa505fbd0c2e9ac4cae72e26739c4b8983b
Jefferson Braga <jeffbraga@gmail.com>
2024-01-25 09:47:41 -0300

    Fix: Realocate to correct place

--- src/unit_test.sh
+++ src/unit_test.sh
@@ -61,6 +61,8 @@ function _check_unit_test_status() {
 # Function to check unit tests
 function _check_unit_test() {
     skip_unit_tests=$(_has_gate_to_skip "unit_test")
+    is_unit_tests_pass=false
+    unit_tests_warn_msg=""
 
     ## Avoid waiting for unit test if not configured
     is_grep_found_step_name=$(grep -qr "name:.*${UNIT_TEST_STEP_NAME}" ${GITHUB_WORKSPACE}/.github/* && echo true || echo false)
@@ -87,9 +89,6 @@ function _check_unit_test() {
         _log "${C_WHT}Checking Unit Test...${C_END}"
         _log "${C_WHT}PR_HEAD_SHA:${C_END} ${PR_HEAD_SHA}"
         _log "${C_WHT}Step name ($UNIT_TEST_STEP_NAME) found in workflow directory [.github]!${C_END}"
-
-        is_unit_tests_pass=false
-        unit_tests_warn_msg=""
 
         workflow_run_id=""
         _retry_with_delay _get_workflow_run_id "$UNIT_TEST_INIT_WAIT_TIMEOUT"
