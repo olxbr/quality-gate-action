6656ef9c649ea6de6ed97948d6cf095bd7a9e57f
Roger Fernandes <roger.osbourne@gmail.com>
2024-03-27 14:05:33 -0300

    refactor: adjust log messages

--- src/quality_gates.sh
+++ src/quality_gates.sh
@@ -8,32 +8,33 @@ function _check_quality_gates() {
     local lock_pull_request=false
 
     if [ "$QUALITY_GATE__UNIT_TEST_PASS" = false ]; then
-        _log error "Unit tests failed!"
+        _log erro "Unit tests failed!"
     fi
 
     if [ "$QUALITY_GATE__CODE_REVIEW_PASS" = false ]; then
-        _log error "Code review failed!"
+        _log erro "Code review failed!"
     fi
 
     if [ "$QUALITY_GATE__OWNER_APPROVAL_PASS" = false ]; then
-        _log error "${C_RED}Owner approval failed!${C_END}"
+        _log erro "Owner approval failed!"
     fi
 
     if [ "$QUALITY_GATE__COVERAGE_PASS" == false ]; then
-        _log error "${C_RED}Coverage failed!${C_END}"
+        _log erro "Coverage failed!"
         lock_pull_request=true
     fi
 
     if [ "$QUALITY_GATE__STATIC_ANALYSIS_PASS" == false ]; then
-        _log error "${C_RED}Static analysis failed!${C_END}"
+        _log erro "Static analysis failed!"
     fi
 
     if [ "$QUALITY_GATE__VULNERABILITY_PASS" == false ]; then
-        _log error "Vulnerability failed!"
+        _log erro "Vulnerability failed!"
     fi
 
     if [ "$lock_pull_request" = true ]; then
         _log error "${C_RED}Pull request is locked!${C_END}"
         echo "::error::Pull request is locked! Please fix the issues and try again."
+        exit 1
     fi
 }
