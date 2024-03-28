3bfda4a46b776d66d861fa2a8b8212c298ef1423
Roger Fernandes <roger.osbourne@gmail.com>
2024-03-28 10:19:10 -0300

    refactor: adjustment in logs and logic

--- src/quality_gates.sh
+++ src/quality_gates.sh
@@ -6,42 +6,41 @@ source "${ACTION_PATH}"/src/utils.sh
 export SKIP_QUALITY_GATE_LOCK="${SKIP_QUALITY_GATE_LOCK:-false}"
 
 function _check_quality_gates() {
-    if [ "$SKIP_QUALITY_GATE_LOCK" = true ]; then
-        _log warn "${C_YEL}Skipping Quality Gate Locks!${C_END}"
-        echo "::warning::Skipping Quality Gate Locks!"
-        return
-    fi
-
     _log "${C_WHT}Checking Quality Gates...${C_END}"
     local lock_pull_request=false
 
     if [ "$QUALITY_GATE__UNIT_TEST_PASS" = false ]; then
-        _log erro "${C_WHT}Unit tests failed!"
+        _log erro "Unit tests failed!"
     fi
 
     if [ "$QUALITY_GATE__CODE_REVIEW_PASS" = false ]; then
-        _log erro "${C_WHT}Code review failed!${C_END}"
+        _log erro "Code review failed!"
     fi
 
     if [ "$QUALITY_GATE__OWNER_APPROVAL_PASS" = false ]; then
-        _log erro "${C_WHT}Owner approval failed!${C_END}"
+        _log erro "Owner approval failed!"
     fi
 
     if [ "$QUALITY_GATE__COVERAGE_PASS" == false ]; then
-        _log erro "${C_WHT}Coverage failed!${C_END}"
+        _log erro "Coverage failed!"
         lock_pull_request=true
     fi
 
     if [ "$QUALITY_GATE__STATIC_ANALYSIS_PASS" == false ]; then
-        _log erro "${C_WHT}Static analysis failed!${C_END}"
+        _log erro "Static analysis failed!"
     fi
 
     if [ "$QUALITY_GATE__VULNERABILITY_PASS" == false ]; then
-        _log erro "${C_WHT}Vulnerability failed!${C_END}"
+        _log erro "Vulnerability failed!"
     fi
 
-    if [ "$lock_pull_request" = true ]; then
-        echo "::error::Pull request is locked! Please fix the issues and try again."
-        exit 1
+    if [ "$SKIP_QUALITY_GATE_LOCK" = true ]; then
+        _log warn "${C_YEL}Skipping Quality Gate Locks!${C_END}"
+        echo "::warning::Quality Gate Locks manually disabled by registering the SKIP_QUALITY_GATE_LOCK variable!"
+    else
+        if [ "$lock_pull_request" = true ]; then
+            echo "::error::Pull Request is locked! Please fix the issues and try again."
+            exit 1
+        fi
     fi
 }
