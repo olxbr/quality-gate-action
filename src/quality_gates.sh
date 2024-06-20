9d90b175b796e765f5ee726260acbe58a06cc874
Roger Fernandes <roger.osbourne@gmail.com>
2024-06-20 14:28:36 -0300

    fix(quality gates): remove quality gate locks

--- src/quality_gates.sh
+++ src/quality_gates.sh
@@ -9,17 +9,14 @@ function _check_quality_gates() {
 
     if [ "$QUALITY_GATE__UNIT_TEST_PASS" = false ]; then
         _log erro "Unit tests failed!"
-        lock_pull_request=true
     fi
 
     if [ "$QUALITY_GATE__CODE_REVIEW_PASS" = false ]; then
         _log erro "Code review failed!"
-        lock_pull_request=true
     fi
 
     if [ "$QUALITY_GATE__OWNER_APPROVAL_PASS" = false ]; then
         _log erro "Owner approval failed!"
-        lock_pull_request=true
     fi
 
     if [ "$QUALITY_GATE__COVERAGE_PASS" == false ]; then
